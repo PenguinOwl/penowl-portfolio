768dcbacfd657d58ec858164de4da0407222a2e6
PenguinOwl <henryjboles@gmail.com>
2018-10-25 18:07:30 -0700

    Finished.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,17 +1,58 @@
 class ProjectsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['RAILS_LOGIN'].to_s, password: ENV['RAILS_PASSWORD'].to_s, except: [:index, :show]
+
+  def index
+    @articles = Article.allr
+  end
+
+  def index
+    @projects = Project.all
+  end
+
   def show
     @project = Project.find(params[:id])
   end
 
   def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
   end
 
   def create
-    @project = Project.new(params.require(:project).permit(:title, :text, :date, :imagelink))
+    @project = Project.new(project_params)
 
-    @project.save
-    redirect_to @project
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :text, :date, :imagelink)
   end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :text, presence: true, length: { minimum: 30 }
+  validates :imagelink, presence: true, length: { minimum: 5 }
 end
