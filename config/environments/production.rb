b3b6729ffe33389ee719196845a1198e457a3d71
PenguinOwl <henryjboles@gmail.com>
2018-11-05 23:01:40 -0800

    Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: ENV['PRODUCTION_DOMAIN'], port: ENV['PORT'] }
+  config.action_mailer.default_url_options = { :host => 'penowl.tk' }
+  ActionMailer::Base.smtp_settings = {
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587,
+    :enable_starttls_auto => true,
+    :authentication       => :plain,
+    :domain               => "penowl.tk"
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
