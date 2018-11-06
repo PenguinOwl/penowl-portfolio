467fb26de0a42c9d7158e99f7a32eed8285df33f
PenguinOwl <henryjboles@gmail.com>
2018-11-06 11:44:13 -0800

    Messages

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,12 @@
 module WelcomeHelper
+  def random_name
+    res = case rand(5)
+    when 0 then "Human"
+    when 1 then "User"
+    when 2 then "Fella"
+    when 3 then "Thinker"
+    when 4 then "Bruce Lee"
+    when 5 then "WW91ciB0aW1lIGhhcyBiZWVuIHdhc3RlZA=="
+    end
+  end
 end
