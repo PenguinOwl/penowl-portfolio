18844400028c966313503ba3a9f37e9dedcb41cc
PenguinOwl <henryjboles@gmail.com>
2018-11-07 07:25:59 -0800

    Better paths for user flags

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-  resources :projects do
+  devise_for :users, path: 'users'
+  resources :users do
     resources :user_flags
   end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  resources :projects
 end
