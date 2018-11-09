1235f62e014c67a03927db66d4289a78c0ac2ab0
PenguinOwl <henryjboles@gmail.com>
2018-11-09 14:11:04 -0800

    Yum

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_11_07_170048) do
     t.bigint "user_id"
     t.bigint "comment_id"
     t.string "content"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["comment_id"], name: "index_comments_on_comment_id"
